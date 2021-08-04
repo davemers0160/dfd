86f17646955cf34a9ee52ce4701c7154091fb392
david emerson <davemers@iu.edu>
2021-08-04 15:27:59 -0400

    more tweaks

--- common/python/blur_calculator/blur_calculator.py
+++ common/python/blur_calculator/blur_calculator.py
@@ -26,7 +26,8 @@ step = 1000
 spin_width = 110
 # px_size = 0.00155
 
-legend_label = ["fp 1", "fp 2", "CoC Difference"]
+# legend_label = ["fp 1", "fp 2", "CoC Difference"]
+legend_label = ["fp 1", "fp 2"]
 
 source = ColumnDataSource(data=dict(x=[], coc=[],  color=[], legend_label=[]))
 stem_source = ColumnDataSource(data=dict(stem_x=[], stem_y0=[], stem_y1=[]))
@@ -40,15 +41,15 @@ f_num = Spinner(title="f number", low=0.01, high=100.0, step=0.01, value=2.35, w
 f = Spinner(title="focal length (mm)", low=0.1, high=2500, step=0.1, value=200, width=spin_width)
 #do_1 = Slider(title="focus point 1 (m):", start=1, end=20000, step=1, value=100, width=1400, callback_policy="mouseup", callback_throttle=50)
 #do_2 = Slider(title="focus point 2 (m):", start=1, end=20000, step=1, value=10000, width=1400, callback_policy="mouseup")
-do_1 = Slider(title="focus point 1 (m):", start=1, end=20000, step=1, value=100, width=1420)
-do_2 = Slider(title="focus point 2 (m):", start=1, end=20000, step=1, value=10000, width=1420)
+do_1 = Slider(title="focus point 1 (m)", start=1, end=20000, step=1, value=100, width=1420)
+do_2 = Slider(title="focus point 2 (m)", start=1, end=20000, step=1, value=10000, width=1420)
 min_x_spin = Spinner(title="Min Range", low=limits[0], high=limits[1], step=1, value=0, width=spin_width)
 max_x_spin = Spinner(title="Max Range", low=limits[0], high=limits[1], step=1, value=1000, width=spin_width)
 min_y_spin = Spinner(title="Min Radius", low=limits[0], high=limits[1], step=1, value=0, width=spin_width)
 max_y_spin = Spinner(title="Max Radius", low=limits[0], high=limits[1], step=1, value=40, width=spin_width)
 div_spin = Spinner(title="Divisions", low=1, high=10000, step=1, value=10, width=spin_width)
-hide_diff_cb = CheckboxGroup(labels=["Show CoC Diff"], active=[0], width=spin_width)
-hide_dups_cb = CheckboxGroup(labels=["Show Duplicates"], active=[0], width=spin_width)
+hide_diff_cb = CheckboxGroup(labels=["Show CoC Diff"], active=[], width=spin_width)
+hide_dups_cb = CheckboxGroup(labels=["Show Duplicates"], active=[], width=spin_width)
 
 #fp1_b = figure(plot_height=200, plot_width=200, title="Focal Point 1", toolbar_location=None)
 #fp1_b.image(image="b1", x=0, y=0, dw=200, dh=200, global_alpha=1.0, dilate=False, palette="Greys256", source=b_source)
@@ -60,13 +61,29 @@ hide_dups_cb = CheckboxGroup(labels=["Show Duplicates"], active=[0], width=spin_
 
 ht = HoverTool(tooltips=[("Range: ", "$x"),  ("Blur Radius: ", "$y{0,0}")], point_policy="snap_to_data", mode="mouse")
 
-coc_plot = figure(plot_height=550, plot_width=1300, title="Quantized Circles of Confusion")
+coc_plot = figure(plot_height=550, plot_width=1300, title="Quantized Circles of Confusion (CoC)")
 l1 = coc_plot.multi_line(xs='x', ys='coc', source=source, line_width=2, color='color', legend_field='legend_label')
 l2 = coc_plot.segment(x0='stem_x', y0='stem_y0', x1='stem_x', y1='stem_y1', source=stem_source, line_width=1, color='red')
 
+coc_plot.title.align = "center"
+coc_plot.title.text_font_size = "16px"
+coc_plot.title.text_font_style = "bold"
+
 coc_plot.xaxis.axis_label = "Range (m)"
-coc_plot.yaxis.axis_label = "Pixel Radius"
-coc_plot.axis.axis_label_text_font_style = "bold"
+coc_plot.xaxis.axis_label_text_font_size = "14px"
+coc_plot.xaxis.axis_label_text_font_style = "bold"
+coc_plot.xaxis.major_label_text_font_size = "14px"
+coc_plot.xaxis.major_label_text_font_style = "bold"
+
+coc_plot.yaxis.axis_label = "Pixel Radius (px)"
+coc_plot.yaxis.axis_label_text_font_size = "14px"
+coc_plot.yaxis.axis_label_text_font_style = "bold"
+coc_plot.yaxis.major_label_text_font_size = "14px"
+coc_plot.yaxis.major_label_text_font_style = "bold"
+
+coc_plot.legend.label_text_font_size = "14px"
+coc_plot.legend.label_text_font_style = "bold"
+
 coc_plot.x_range = Range1d(start=min_x_spin.value, end=max_x_spin.value)
 coc_plot.y_range = Range1d(start=min_y_spin.value, end=max_y_spin.value)
 # coc_plot.legend[0].location = (900, 200))
@@ -111,7 +128,7 @@ update_plot_callback = CustomJS(args=cb_dict, code="""
     var prev_coc = 0;
     var curr_coc = 0;
     
-    console.log("test")
+    //console.log("test")
     
     for(var idx = 0; idx<num; idx++)
     {
@@ -218,7 +235,7 @@ def update_plot(attr, old, new):
     q_coc_slice = q_coc_diff[0:-1:div_spin.value]
     diff_match = np.hstack(([False], (q_coc_slice[0:-1] == q_coc_slice[1:])))
 
-    source.data = dict(x=[r, r, r], coc=[q_coc1, q_coc2, q_coc_diff], color=['blue', 'green', 'black'], legend_label=["fp 1", "fp 2", "CoC Difference"])
+    source.data = dict(x=[r, r, r], coc=[q_coc1, q_coc2, q_coc_diff], color=['blue', 'green', 'black'], legend_label=["fp 1", "fp 2", "CoC Difference"]) #, "CoC Difference"
     stem_source.data = dict(stem_x=r_slice[diff_match], stem_y0=np.zeros(r_slice[diff_match].shape[0]), stem_y1=np.full(r_slice[diff_match].shape[0], max_y_spin.value))
     # source.data = dict(x=[r], coc1=[q_coc1], coc2=[q_coc2], coc_diff=[q_coc_diff])
     #b_source.data = dict(b1=[blur_alpha], b2=[blur_alpha])
